318dd8c6a04abce63e136d3452ccf0e395041527
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-02-09 15:13:51 +0100

    controllo

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ new Vue({
 		let self=this
 		axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&query=ritorno')
               .then(function(element) {
-               const arrayVideo=(element.data.response);
+               const arrayVideo=element.data.response;
 			   self.boxListFilms=arrayVideo;  
               });
        
