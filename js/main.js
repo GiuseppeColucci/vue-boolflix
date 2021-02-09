047a2bbc357b54a2d0173627c90f41bfa9da647c
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-02-09 15:27:29 +0100

    controllo parte di ricerca

--- js/main.js
+++ js/main.js
@@ -6,14 +6,22 @@ new Vue({
     },
     methods:{
         
+        cerca:function(){
+            let self=this
+            axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&+search')
+                  .then(function(element) {
+                   const arrayVideo=element.data.results;
+                   self.boxListFilms=arrayVideo;  
+                  });
+        }
     },
     mounted() {
-		let self=this
-		axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&query=ritorno')
-              .then(function(element) {
-               const arrayVideo=element.data.response;
-			   self.boxListFilms=arrayVideo;  
-              });
+		// let self=this
+		// axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&query=ritorno')
+        //       .then(function(element) {
+        //        const arrayVideo=element.data.results;
+		// 	   self.boxListFilms=arrayVideo;  
+        //       });
        
       }
 
