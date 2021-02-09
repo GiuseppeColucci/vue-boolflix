1dd290815293bb192b8dfff82301f2d767ee4d0c
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-02-09 15:34:45 +0100

    finita la prima parte del poject

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ new Vue({
         
         cerca:function(){
             let self=this
-            axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&+search')
+            axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&query=' + this.search)
                   .then(function(element) {
                    const arrayVideo=element.data.results;
                    self.boxListFilms=arrayVideo;  
@@ -16,12 +16,7 @@ new Vue({
         }
     },
     mounted() {
-		// let self=this
-		// axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&query=ritorno')
-        //       .then(function(element) {
-        //        const arrayVideo=element.data.results;
-		// 	   self.boxListFilms=arrayVideo;  
-        //       });
+
        
       }
 
