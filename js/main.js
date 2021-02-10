34e548c3cdae036e38c8423c14573cacf374cca7
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-02-10 11:15:03 +0100

    finita la milestone 2

--- js/main.js
+++ js/main.js
@@ -2,16 +2,30 @@ new Vue({
     el:'#app',
     data:{
         boxListFilms:[],
+        boxSerieTv:[],
         search:'',
     },
     methods:{
-        
-        cerca:function(){
+        totalSearch:function(){
+            this.cercaFilms();
+            this.cercaSerieTV();
+        },
+        //parte Films
+        cercaFilms:function(){
             let self=this
             axios.get(' https://api.themoviedb.org/3/search/movie?api_key=4950e05cc9431b58c3a17d768d6eea1b&query=' + this.search)
                   .then(function(element) {
                    const arrayVideo=element.data.results;
                    self.boxListFilms=arrayVideo;  
+                  });
+        },
+        //parte serie tv
+        cercaSerieTV:function(){
+            let self=this
+            axios.get(' https://api.themoviedb.org/3/search/tv?api_key=4950e05cc9431b58c3a17d768d6eea1b&language=it_IT&query=' + this.search)
+                  .then(function(element) {
+                   const arraySerieTv=element.data.results;
+                   self.boxSerieTv=arraySerieTv;  
                   });
         },
         //parte voto
